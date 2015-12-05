9770410933db8c5a1d04e3f118136e3df7c322e7
balajikr <balaji.kasi.rajamani@gmail.com>
2015-12-06 00:29:55 +0530

    base setup code

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name redmartApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the redmartApp
+ */
+angular.module('redmartApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
